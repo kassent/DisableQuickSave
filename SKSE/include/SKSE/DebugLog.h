b83bc2fcf8679ecf8b38028717c1f829263094af
kassent <wangzhengzewzz@gmail.com>
2017-03-26 01:16:12 +0800

    Add project files.

---
+++ SKSE/include/SKSE/DebugLog.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <fstream>
+#include <Skyrim\BSCore\BSCoreTypes.h>
+
+#if _DEBUG
+#define DEBUG_MESSAGE(str) do { gLog << str << std::endl; gLog.flush(); } while (false)
+#else
+#define DEBUG_MESSAGE(str) do { } while (false)
+#endif
+
+class DebugLog : public std::ofstream
+{
+public:
+	DebugLog();
+
+	void operator()(const char* message);
+	void operator()(const char* message, ...);
+};
+
+void _MESSAGE(const char* fmt, ...);
+
+extern DebugLog gLog;
